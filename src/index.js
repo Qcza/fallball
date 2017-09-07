bb2e0e3f8492b3c413f379b4169256caaec8e451
pku <pawel.kuczynski@ue.wroc.pl>
2017-09-07 14:31:52 +0200

    Added obstacles.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('./main.scss');
 
 import ball from './assets/ball.png'
 import coin from './assets/coin.png'
+import obstacle01 from './assets/obstacle01.png'
 
 import 'pixi';
 import 'p2';
@@ -13,11 +14,12 @@ const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   update: update
 });
 
-let player, cursors, coins, score, scoreText, winText, loseText, countdown, ko;
+let player, cursors, coins, obstacles, score, scoreText, winText, loseText;
 
 function preload () {
   game.load.image('ball', ball);
   game.load.spritesheet('coin', coin, 32, 29);
+  game.load.image('obstacle01', obstacle01);
 }
 
 function create () {
@@ -25,6 +27,22 @@ function create () {
   game.stage.backgroundColor = "#CCC";
   scoreText = game.add.text(16, 16, 'Collected 0 coins.', { fontSize: '32px', fill: '#000' });
   player = game.add.sprite(400, 200, 'ball');
+
+  obstacles = game.add.group();
+  obstacles.enableBody = true;
+
+  for (let o = 1; o<=12; o++) {
+
+    const obstacle = obstacles.create(Math.floor(Math.random()*700)+1,(Math.floor(Math.random()*500)+50),'obstacle01');
+    game.physics.arcade.overlap(obstacle, obstacles, ()=>{
+      obstacle.kill();
+      o--
+    }, null, this);
+    // coin.body.gravity.y = 300;
+    // coin.body.bounce.y = 0.7 + Math.random() * 0.2;
+    // coin.body.collideWorldBounds = true;
+  }
+
   coins = game.add.group();
   coins.enableBody = true;
 
@@ -34,10 +52,17 @@ function create () {
     coin.animations.add('spin');
     coin.animations.play('spin', 30, true)
 
+    game.physics.arcade.overlap(coin, obstacles, ()=>{
+      coin.kill();
+      i--
+    }, null, this);
+
     // coin.body.gravity.y = 300;
     // coin.body.bounce.y = 0.7 + Math.random() * 0.2;
     // coin.body.collideWorldBounds = true;
   }
+
+
   game.physics.arcade.enable(player);
 
   // player.body.bounce.y = .5;
@@ -52,6 +77,7 @@ function update () {
   player.body.velocity.x = 0;
 
   game.physics.arcade.overlap(player, coins, getCoin, null, this);
+  game.physics.arcade.overlap(player, obstacles, boundsKills, null, this);
 
   if (cursors.left.isDown)
   {
@@ -80,7 +106,7 @@ function getCoin (player, coin) {
   coin.kill();
   score += 1;
   scoreText.text = `Collected ${score} coins.`;
-  if (score === coins.length) {
+  if (score === 10) {
     game.add.text(250, 250, 'YOU WIN!', { fontSize: '64px', fill: '#000' });
   }
 }
@@ -88,4 +114,8 @@ function getCoin (player, coin) {
 function boundsKills () {
   game.stage.backgroundColor = "#F00";
   game.state.restart();
+}
+
+function checkIfOverlap (obstacle) {
+
 }
